bd3d32d7819414abfd16c4371f099b42fdf82123
scottwegley <scottwegley@gmail.com>
2022-09-29 01:31:28 -0700

    Utility to display GB-TB-PB between each other

---
+++ libraries/utils/ram_convert.js
@@ -0,0 +1,11 @@
+export function toReadable(inN,type = 'g'){
+	let suffix = ((type == 'g') ? 'GB' : ((type == 't') ? 'TB' : 'PB'));
+	if(inN < 1024) return inN + suffix;
+	else if(inN >= Math.pow(2,20)) {
+		return inN / 1024 / 1024 + 'PB';
+	}
+	else {
+		suffix = ((type == 'g') ? 'TB' : 'PB');
+		return inN / 1024 + suffix;
+	}
+}
