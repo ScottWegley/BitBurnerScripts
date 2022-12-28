c8389667db0e17c1795fa3d9a5b5c23b2579c590
scottwegley <scottwegley@gmail.com>
2022-12-28 02:55:26 -0800

    decrement through potential server ram if at any point we can afford to buy, buy

--- src/scripts/attempt_upgrade.ts
+++ src/scripts/attempt_upgrade.ts
@@ -1,7 +1,17 @@
 import { NS, Server } from "@ns";
 
 /** @param {NS} ns */
-export async function main(ns:NS) {
+export async function main(ns: NS) {
 	var ramPower = 20;
+	var servers = ns.getPurchasedServers();
 
+	for (ramPower; ramPower > 0; ramPower--) {
+		if (ns.getServerMoneyAvailable('home') > ns.getPurchasedServerCost(Math.pow(2, ramPower))) {
+			if (servers.length < ns.getPurchasedServerLimit()) { //If we have space for a new server
+				var newServerName = ("Server_" + (servers.length + 1));
+				ns.tprint("Purchasing a server: " + newServerName);
+				ns.purchaseServer(("Server_" + (servers.length + 1)), Math.pow(2, ramPower));
+				return;
+			}
+	}
 }
