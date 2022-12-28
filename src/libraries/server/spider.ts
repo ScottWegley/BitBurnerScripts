fa065911563a584e1f767f5a4a1a3f16ad21316c
scottwegley <scottwegley@gmail.com>
2022-12-28 02:43:23 -0800

    loop through and save all servers also indent servers as added

--- src/libraries/server/spider.ts
+++ src/libraries/server/spider.ts
@@ -12,4 +12,19 @@ export async function spider(ns: NS, log: boolean = true, raw: boolean = true, t
 
 	ns.disableLog('scan');
 
+	for (let i = 0; i < tagCheck.length; i++) {
+		let newScan = await ns.scan(tagCheck[i]);
+		for (let j = newScan.length - 1; j >= 0; j--) {
+			if (tagCheck.indexOf(newScan[j]) === -1) { //If the server in the scan has not been tagged yet.
+				let indent = tagged[i].substring(0, tagged[i].lastIndexOf('_') + 1) + indentMod;
+				let preString = indent.substring(0, indent.lastIndexOf('|')).length;
+				indent = indent.substring(preString, indent.length);
+				for (let q = 0; q < preString; q++) {
+					indent = " " + indent;
+				}
+				tagged.splice(i + 1, 0, indent + newScan[j]);
+				tagCheck.splice(i + 1, 0, newScan[j]);
+			}
+		}
+	}
 }
