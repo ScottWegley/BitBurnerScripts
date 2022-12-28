90a5ce0b1128b01f600e23e24ff87e0d1589eedd
scottwegley <scottwegley@gmail.com>
2022-12-28 02:40:00 -0800

    empty function with params

---
+++ src/libraries/server/spider.ts
@@ -0,0 +1,9 @@
+import { NS } from "@ns";
+
+/**
+ * @param {boolean} log - True saves a tree of the scan to the script logs.
+ * @param {boolean} raw - True returns just an array of servers, false returns it with tree indents.
+ * @param {boolean} terminal - True prints the tree to the terminal.
+*/
+export async function spider(ns: NS, log: boolean = true, raw: boolean = true, terminal = false) {
+}
