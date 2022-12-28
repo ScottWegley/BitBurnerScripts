299eb729614aaa68e721ede655025edc7b23b441
scottwegley <scottwegley@gmail.com>
2022-12-28 02:36:10 -0800

    removing js for typescript conversion and refactor

--- terminal-utils/lsx.js
+++
@@ -1,7 +0,0 @@
-/** @type import(".").NS */
-
-/** @param {NS} ns */
-export async function main(ns) { //Lists every file on computer instead of current directory
-	var root = (ns.ls('home'));
-	root.forEach(i => ns.tprint(i + "\n"));
-}
