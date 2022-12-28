763dca9cfffb82fb7bf77cbd4769f6f81cf7f045
scottwegley <scottwegley@gmail.com>
2022-12-28 03:02:35 -0800

    format output based on level of detail and oritentation orientation needs vertical still

--- src/libraries/server/server_report.ts
+++ src/libraries/server/server_report.ts
@@ -14,6 +14,43 @@ export async function report(ns: NS, serverName: string, lOD:number , print = tr
     var server: Server = (typeof serverName === 'string' && ns.serverExists(serverName) ? ns.getServer(serverName) : ns.getServer('home'));
     try {
         let printout: string = ' ';
+        switch (lOD) {
+            default:
+                if (tall) {
+
+                } else {
+                    printout = `${server.hostname} || Root: ${server.hasAdminRights} || Ports: ${server.numOpenPortsRequired} || Cracked: ${server.openPortCount} || RAM: ${toReadable(server.maxRam)} || Pot. ${ns.nFormat(server.moneyMax, '($0.00a)').toUpperCase()}`;
+                }
+                break;
+            case 2:
+                if (tall) {
+
+                } else {
+                    printout = `${server.hostname} || Root: ${server.hasAdminRights} || Backdoored: ${server.backdoorInstalled} || Ports: ${server.numOpenPortsRequired} || Cracked: ${server.openPortCount} || RAM: ${toReadable(server.ramUsed)}/${toReadable(server.maxRam)} ${server.ramUsed / server.maxRam * 100}% || Pot. ${ns.nFormat(server.moneyAvailable, '($0.00a)').toUpperCase()}/${ns.nFormat(server.moneyMax, '($0.00a)').toUpperCase()} ${(server.moneyMax != 0 ? server.moneyAvailable / server.moneyMax * 100 : 0)}%`;
+                }
+                break;
+            case 3:
+                if (tall) {
+
+                } else {
+                    printout = `${server.hostname} || Root: ${server.hasAdminRights} || Backdoored: ${server.backdoorInstalled} || Ports: ${server.numOpenPortsRequired} || Cracked: ${server.openPortCount} || Security: ${ns.getServerSecurityLevel(server.hostname)} || RAM: ${toReadable(server.ramUsed)}/${toReadable(server.maxRam)} ${server.ramUsed / server.maxRam * 100}% || Pot. ${ns.nFormat(server.moneyAvailable, '($0.00a)').toUpperCase()}/${ns.nFormat(server.moneyMax, '($0.00a)').toUpperCase()} ${(server.moneyMax != 0 ? server.moneyAvailable / server.moneyMax * 100 : 0)}%`;
+                }
+                break;
+            case 4:
+                if (tall) {
+
+                } else {
+                    printout = `${server.hostname} || Root: ${server.hasAdminRights} || Backdoored: ${server.backdoorInstalled} || Ports: ${server.numOpenPortsRequired} || Cracked: ${server.openPortCount} || Security: ${ns.getServerSecurityLevel(server.hostname)} || RAM: ${toReadable(server.ramUsed)}/${toReadable(server.maxRam)} ${server.ramUsed / server.maxRam * 100}% || Hack Time: ${(ns.getHackTime(server.hostname)/1000).toFixed(2)} sec. || Grow Time: ${(ns.getGrowTime(server.hostname)/1000).toFixed(2)} sec. || Weak Time: ${(ns.getWeakenTime(server.hostname)/1000).toFixed(2)} sec. || Pot. ${ns.nFormat(server.moneyAvailable, '($0.00a)').toUpperCase()}/${ns.nFormat(server.moneyMax, '($0.00a)').toUpperCase()} ${(server.moneyMax != 0 ? server.moneyAvailable / server.moneyMax * 100 : 0)}%`;
+                }
+                break;
+            case 5:
+                if (tall) {
+
+                } else {
+                    printout = `${server.hostname} || Root: ${server.hasAdminRights} || Backdoored: ${server.backdoorInstalled} || Ports: ${server.numOpenPortsRequired} || Cracked: ${server.openPortCount} || FTP: ${server.ftpPortOpen} || SSH: ${server.sshPortOpen} || HTTP: ${server.httpPortOpen} || SQL: ${server.sqlPortOpen} || SMTP: ${server.smtpPortOpen} || Security: ${ns.getServerSecurityLevel(server.hostname)} || RAM: ${toReadable(server.ramUsed)}/${toReadable(server.maxRam)} ${server.ramUsed / server.maxRam * 100}% || Hack Time: ${(ns.getHackTime(server.hostname)/1000).toFixed(2)} sec. || Grow Time: ${(ns.getGrowTime(server.hostname)/1000).toFixed(2)} sec. || Weak Time: ${(ns.getWeakenTime(server.hostname)/1000).toFixed(2)} sec. || Pot. ${ns.nFormat(server.moneyAvailable, '($0.00a)').toUpperCase()}/${ns.nFormat(server.moneyMax, '($0.00a)').toUpperCase()} ${(server.moneyMax != 0 ? server.moneyAvailable / server.moneyMax * 100 : 0)}%`;
+                }
+                break;
+        }
         printout = printout.replaceAll('true',ucChars.check).replaceAll('false',ucChars.x);
         if (print) {
             ns.print(printout);
