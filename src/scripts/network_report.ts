64c71103fe8fe6f86b82f2f05e1607786c618f34
scottwegley <scottwegley@gmail.com>
2022-12-29 23:13:02 -0800

    added param checks

--- src/scripts/network_report.ts
+++ src/scripts/network_report.ts
@@ -8,8 +8,16 @@ import { report } from "libraries/server/server_report";
 export async function main(ns: NS) {
 	network_report(ns, (typeof ns.args[0] === 'number' ? ns.args[0] : 1), ns.args);
 }
+
 export async function network_report(ns: NS, lOD: number, args: (string | number | boolean)[]) {
+	if (!(lOD >= 1 && lOD <= 5)) lOD = 1;
+	args.splice(0, 1);
+	var returnArray: string[] = [];
 	var serversArray = await spider(ns, false, true, false);
+	if (args.length % 2 != 0) {
+		ns.tprint('Invalid Arguments!');
+		return;
+	}
 	serversArray.forEach(async x => {
 		var server = ns.getServer(x);
 		var name = server.hostname;
