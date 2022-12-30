bb5e3cb02a14f9eda7b6bbe627fc9209ab639f85
scottwegley <scottwegley@gmail.com>
2022-12-29 23:10:39 -0800

    baby script to deploy baby hack to rooted servers

---
+++ src/managers/babybotnet.ts
@@ -0,0 +1,15 @@
+import { NS } from "@ns";
+
+import { spider } from "libraries/server/spider";
+
+/** @param {NS} ns */
+export async function main(ns:NS) {
+    let servers = await spider(ns, false);
+    servers.forEach((x:string|undefined) =>{
+        let s = ns.getServer(x);
+        if(s.hasAdminRights && !s.purchasedByPlayer){
+            ns.rm('/scripts/primativehacking.js',x!)
+            ns.scp('/scripts/primativehacking.js',x!);
+        }
+    })
+}
